3c538ed22164abbb9381aac432b33a6ad4d8d338
JoksLobo <jocson_herry@hotmail.com>
2024-09-23 17:15:15 -0300

    commit inicial, criação de components, containers

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,28 @@
+import Tarefa from '../../components/Tarefa'
+
+const ListaDeTarefas = () => (
+  <main>
+    <p>
+      2 tarefas marcadas como: &quot;categoria&ldquot; e &quot;termo&ldquot;
+    </p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </main>
+)
+
+export default ListaDeTarefas
